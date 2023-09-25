562478a158d7c70d17dce52270aa0adb468d53c2
SandWithCheese <naufalahmad022@gmail.com>
2023-09-25 20:57:19 +0700

    feat: added more methods to matrix class

--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -36,4 +36,66 @@ public class Matrix {
             System.out.println(String.format("%8s", "|"));
         }
     }
+
+    public int getRow() {
+        return this.row;
+    }
+
+    public int getCol() {
+        return this.col;
+    }
+
+    public double getElement(int row, int col) {
+        return this.data[row][col];
+    }
+
+    public boolean isMatrixIdxValid(int row, int col) {
+        return row >= 0 && row < this.row && col >= 0 && col < this.col;
+    }
+
+    public Matrix copyMatrix() {
+        Matrix copy = new Matrix(this.row, this.col);
+
+        for (int i = 0; i < this.row; i++) {
+            System.arraycopy(this.data[i], 0, copy.data[i], 0, this.col);
+        }
+
+        return copy;
+    }
+
+    public boolean isSquareMatrix() {
+        return this.row == this.col;
+    }
+
+    public boolean isIdentity() {
+        if (!this.isSquareMatrix()) {
+            return false;
+        }
+
+        for (int i = 0; i < this.row; i++) {
+            if (this.data[i][i] != 1) {
+                return false;
+            }
+
+            for (int j = 0; j < this.col; j++) {
+                if (i != j && this.data[i][j] != 0) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public Matrix transpose() {
+        Matrix transpose = new Matrix(this.col, this.row);
+
+        for (int i = 0; i < this.col; i++) {
+            for (int j = 0; j < this.row; j++) {
+                transpose.data[i][j] = this.data[j][i];
+            }
+        }
+
+        return transpose;
+    }
 }
--- test/Main.java
+++ test/Main.java
@@ -2,8 +2,12 @@ import matrix.Matrix;
 
 public class Main {
     public static void main(String[] args) {
-        Matrix M1 = new Matrix(3, 3);
+        Matrix M1 = new Matrix(2, 3);
         M1.readMatrix();
         M1.displayMatrix();
+
+        Matrix M2 = M1.copyMatrix();
+
+        M2.transpose().displayMatrix();
     }
 }
