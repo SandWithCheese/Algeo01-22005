4c151794ec6213d0dc66a1eb828d998ce7b5e10f
XAVAX ​ <mdavaf17@gmail.com>
2023-09-27 10:00:19 +0700

    feat: added mtxCofactor, mtxAdj, Inverse by adjoint

--- src/matrix/balikan/Adjoin.java
+++ src/matrix/balikan/Adjoin.java
@@ -1,5 +1,41 @@
 package matrix.balikan;
 
-public class Adjoin {
+import matrix.Matrix;
+import matrix.determinan.Kofaktor;
 
+public class Adjoin {
+    public Matrix MtxKofaktor(Matrix M, int row, int col){
+        Matrix result = new Matrix(row, col);
+
+        Kofaktor Adj = new Kofaktor();
+        int sign = 1;
+
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < col; j++){
+                result.setElement(i, j, sign * Adj.detKofaktor(Adj.Minor(M, i, j)));
+                sign *= (-1);
+            }
+        }
+
+        return result;
+    }
+
+    public Matrix MtxAdjoin(Matrix M, int row, int col){
+        Matrix result = new Adjoin().MtxKofaktor(M, row, col);
+
+        return result.transpose();        
+    }
+
+    public Matrix inverseAdjoin(Matrix M, int row, int col){
+        Matrix result = new Adjoin().MtxAdjoin(M, row, col);
+        double det = new Kofaktor().detKofaktor(M);
+
+        for (int i = 0; i < row; i++){
+            for (int j = 0; j < col; j++){
+                result.setElement(i, j, result.getElement(i, j) * ((float)1 / det));
+            }
+        }
+
+        return result;
+    }
 }
