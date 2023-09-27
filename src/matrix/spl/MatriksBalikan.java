52edf20d5884e21911432b123668fcc2733c70f3
XAVAX ​ <mdavaf17@gmail.com>
2023-09-27 22:48:57 +0700

    feat: added SPL by inverse

--- src/matrix/spl/MatriksBalikan.java
+++ src/matrix/spl/MatriksBalikan.java
@@ -1,5 +1,38 @@
 package matrix.spl;
 
+import matrix.Matrix;
+import matrix.balikan.Adjoin;
+
 public class MatriksBalikan {
-    
+    public Matrix multiplyMatrix(Matrix m1, Matrix m2){
+        Matrix Mout = new Matrix(m1.getRow(), m2.getCol());
+
+        for (int i = 0; i < m1.getRow(); i++){
+            for (int j = 0; j < m2.getCol(); j++){
+                Mout.setElement(i, j, 0);
+
+                for (int k = 0; k < m1.getCol(); k++){
+                    double valMout = Mout.getElement(i, j);
+                    double valM1 = m1.getElement(i, k);
+                    double valM2 = m2.getElement(k, j);
+
+                    Mout.setElement(i, j, valMout + valM1 * valM2);
+                }
+            }
+        }
+
+        return Mout;
+    }
+
+    public Matrix SPLInverse(Matrix A, Matrix B){
+        Matrix Ai = new Adjoin().inverseAdjoin(A);
+
+        if (Ai != null){
+            Matrix result = multiplyMatrix(Ai, B);
+            return result;
+        }
+        else{
+            return null;
+        }
+    }
 }
