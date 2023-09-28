532dc9cf24d442d81b3770a8cdfdf18789868655
saadabha <saadabdulhakim08@gmail.com>
2023-09-28 10:21:31 +0700

    feat: added Inverse using Gauss Jordan

---
+++ src/matrix/balikan/BalikanGaussJordan.java
@@ -0,0 +1,40 @@
+package matrix.balikan;
+
+import matrix.Matrix;
+import matrix.spl.GaussJordan;
+
+public class BalikanGaussJordan {
+    public Matrix balikanGaussJordan(Matrix m) {
+        Matrix sementara = new Matrix(m.getRow(), 2*m.getCol());
+        Matrix hasil = new Matrix(m.getRow(), m.getCol());
+        
+        for(int i=0;i<sementara.getRow();i++){
+            for(int j=0;j<sementara.getCol();j++){
+                if (j<m.getCol()){
+                    sementara.setElement(i, j, m.getElement(i, j));
+                } else {
+                    if (j-m.getCol()==i){
+                        sementara.setElement(i, j, 1);
+                    } else {
+                        sementara.setElement(i, j, 0);
+                    }
+                }
+            }
+        }
+
+        GaussJordan gaussjordan = new GaussJordan();
+        sementara = gaussjordan.gaussJordan(sementara);
+
+        int col,row = 0;
+        for(int i=0;i<sementara.getRow();i++){
+            col = 0;
+            for(int j=m.getCol();j<sementara.getCol();j++){
+                hasil.setElement(row, col, sementara.getElement(i, j));
+                col++;
+            }
+            row++;
+        }
+        
+        return hasil;
+    }
+}
